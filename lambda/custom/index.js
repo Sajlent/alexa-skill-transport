5877e0118c752516fc5dbedd53efc9c648d9faa0
Agata Zielińska <agata.zielinska@k2.pl>
2019-03-09 13:38:34 +0100

    Get users input

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -8,8 +8,8 @@ const cookbook = require('./alexa-cookbook.js');
 //TODO: The items below this comment need your attention.
 //=========================================================================================================================================
 
-const SKILL_NAME = 'Space Facts';
-const GET_FACT_MESSAGE = 'Here\'s your fact: ';
+const SKILL_NAME = 'Transport in London';
+const GET_ROUTE_MESSAGE = 'Here\'s your route: ';
 const HELP_MESSAGE = 'You can say tell me a space fact, or, you can say exit... What can I help you with?';
 const HELP_REPROMPT = 'What can I help you with?';
 const FALLBACK_MESSAGE = 'The Space Facts skill can\'t help you with that.  It can help you discover facts about space if you say tell me a space fact. What can I help you with?';
@@ -21,42 +21,35 @@ const STOP_MESSAGE = 'Goodbye!';
 //=========================================================================================================================================
 
 const data = [
-  'A year on Mercury is just 88 days long.',
-  'Despite being farther from the Sun, Venus experiences higher temperatures than Mercury.',
-  'Venus rotates counter-clockwise, possibly because of a collision in the past with an asteroid.',
-  'On Mars, the Sun appears about half the size as it does on Earth.',
-  'Earth is the only planet not named after a god.',
-  'Jupiter has the shortest day of all the planets.',
-  'The Milky Way galaxy will collide with the Andromeda Galaxy in about 5 billion years.',
-  'The Sun contains 99.86% of the mass in the Solar System.',
-  'The Sun is an almost perfect sphere.',
-  'A total solar eclipse can happen once every 1 to 2 years. This makes them a rare event.',
-  'Saturn radiates two and a half times more energy into space than it receives from the sun.',
-  'The temperature inside the Sun can reach 15 million degrees Celsius.',
-  'The Moon is moving approximately 3.8 cm away from our planet every year.',
+  'The first stop is Westminster, the last is Bank. You will travel directly.',
+  'Travel from Westminster to Bank takes 13 minutes.'
 ];
 
 //=========================================================================================================================================
 //Editing anything below this line might break your skill.
 //=========================================================================================================================================
 
-const GetNewFactHandler = {
+const GetTravelStopsHandler = {
   canHandle(handlerInput) {
     const request = handlerInput.requestEnvelope.request;
     return request.type === 'LaunchRequest'
       || (request.type === 'IntentRequest'
-        && request.intent.name === 'GetNewFactIntent');
+        && request.intent.name === 'GetTravelRoute');
   },
   handle(handlerInput) {
-    const randomFact = cookbook.getRandomItem(data);
-    const speechOutput = GET_FACT_MESSAGE + randomFact;
+    const slots = handlerInput.requestEnvelope.request.intent.slots;
+    const from = slots['from'].value;
+    const to = slots['to'].value;
+    //const randomFact = cookbook.getRandomItem(data);
+    const speechOutput = `Here's your route: from ${from} to ${to}`;
 
     return handlerInput.responseBuilder
       .speak(speechOutput)
-      .withSimpleCard(SKILL_NAME, randomFact)
+      .withSimpleCard(SKILL_NAME, speechOutput)
       .getResponse();
   },
 };
+
 
 const HelpHandler = {
   canHandle(handlerInput) {
@@ -133,7 +126,7 @@ const skillBuilder = Alexa.SkillBuilders.custom();
 
 exports.handler = skillBuilder
   .addRequestHandlers(
-    GetNewFactHandler,
+    GetTravelStopsHandler,
     HelpHandler,
     ExitHandler,
     FallbackHandler,
